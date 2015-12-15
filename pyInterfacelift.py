aa0083798773789310b815dfbd42c716bdf408f3
Henrik Stroetgen <hstroetgen@synapticon.com>
2015-12-15 10:09:51 +0100

    works now, no features at the moment

--- pyInterfacelift.py
+++ pyInterfacelift.py
@@ -1,20 +1,19 @@
 import re
 import requests
+import argparse
 
 __author__ = 'BlexTw11'
-__version__ = '0.1'
+__version__ = '1.0'
 url_start = "https://interfacelift.com/"
 url_start_wp = "https://interfacelift.com/wallpaper/"
 url = 'https://interfacelift.com/wallpaper/downloads/date/index1.html'
-url_download_fix = "/7yz4ma1/"
+url_download_fix = "7yz4ma1/"
 
 def id_parser(r):
-    #return re.search(r'id="list_(?P<id>[\d]+)"', r.content).group('id')
     return re.findall(r'id="list_([\d]+)"', r.content)
 
 def name_parser(r, id):
     return re.search(r'javascript\:imgload\(\'(?P<name>.+)\'\, this\,\'%s\'\)' % id, r.content).group('name')
-    #return re.findall(r'javascript\:imgload\(\'(.+)\'\, this\,\'%s\'\)' % id, r.content)
 
 def next_page(r):
     link = re.search(r'href=\"(?P<link>.+\.html)\".+\>next page ', r.content).group('link')
@@ -25,46 +24,46 @@ def last_page(r):
     return True if link else False
 
 def find_resolution(r, id, resolution):
-    res = re.search(
-            r'\<div id=\"list_%s\"\>.*%s.*\<div style=\"clear: both; padding: 0; margin: 0; height: 0;\"\>\</div\>'
-            % (id, resolution), r.content)
-    print res
+    regex = r'%s[\'\"\)\>\<\w\d\s\=:\/]*?Select Resolution[\'\"\)\>\<\w\d\s\=:\/_,\(.\-;]*?%s' % (id, resolution)
+    res = re.search(regex, r.content)
     return True if res else False
 
-def load_files(r, id, name, resolution):
+def load_files(r, id, name, resolution, path):
 
-    file_name = "%05d_%s_%s.jpg" % (id, name, resolution)
-    print file_name
+    file_name = "%05d_%s_%s.jpg" % (int(id), name, resolution)
     url = url_start_wp + url_download_fix + file_name
-    print url
 
     r_file = requests.get(url)
     # Schreibe die Daten in Dateien
-    with open(file_name, "wb") as pic:
+    with open(path + "/" + file_name, "wb") as pic:
         pic.write(r_file.content)
 
     if r_file.status_code != requests.codes.ok:
         raise ValueError('Download picture failed')
 
 def main():
-    #parser = argparse.ArgumentParser(description='pyInterfaceLift v' + __version__ + ' by ' + __author__)
-    #parser.add_argument('-d', '--debug', action='store_true', help='Debug mode. Downloads not really a file.', dest='debug')
-    #parser.add_argument('-s', '--single', action='store_true', help='Download just a single file.', dest='single')
-    #args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='pyInterfaceLift v' + __version__ + ' by ' + __author__)
+    parser.add_argument('resolution', help='Defines the resolution. E.g. 1920x1080')
+    parser.add_argument('path', help='Defines the path where the wallpapers will be stored.')
+    args = parser.parse_args()
 
     r = requests.get(url)
 
-    resolution = '3840x2160'
-
+    resolution = args.resolution
+    path = args.path
+    page_counter = 1
     while not last_page(r):
         ids = id_parser(r)
+        print "Page:", page_counter
+        page_counter += 1
         for id in ids:
             print "Image ID:", id
             name = name_parser(r, id)
             print "Image Name:", name
             if find_resolution(r, id, resolution):
-                load_files(r, id, name, resolution)
-
+                load_files(r, id, name, resolution, path)
+            print
+        r = next_page(r)
 
 
 if __name__ == '__main__':
